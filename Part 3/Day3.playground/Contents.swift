72ef74c2051383f628f7fda25f8513b9d2f6b760
rbdannie <rui026@my.utsa.du>
2022-01-27 16:31:17 -0600

    operators and conditions

---
+++ Part 3/Day3.playground/Contents.swift
@@ -0,0 +1,114 @@
+import UIKit
+
+
+//Arithmetic Operators
+let firstScore = 12
+let secondScore = 4
+
+let total = firstScore + secondScore
+let difference = firstScore - secondScore
+
+let product = firstScore * secondScore
+let divided = firstScore / secondScore
+
+let remainder = 13 % secondScore
+
+
+//Operator overloading
+let meaningOfLife = 42
+let doubleMeaning = 42 + 42
+
+let fakers = "Fakers gonna "
+let action = fakers + "fake"
+
+let firstHalf = ["John", "Paul"]
+let secondHalf = ["George", "Ringo"]
+let beatles = firstHalf + secondHalf
+
+
+//Compound assignment operators
+var testscore = 95
+testscore -= 5
+
+var quote = "The rain in Spain falls mainly on the "
+quote += "Spaniards"
+
+
+//Comparison Operators
+firstScore == secondScore
+firstScore != secondScore
+
+firstScore < secondScore
+firstScore >= secondScore
+
+"Taylor" <= "Swift"
+
+
+//Conditions
+let firstCard = 11
+let secondCard = 10
+
+if firstCard + secondCard == 2 {
+    print("Aces – lucky!")
+} else if firstCard + secondCard == 21 {
+    print("Blackjack!")
+} else {
+    print("Regular cards")
+}
+
+
+
+//Combining Conditions
+let age1 = 12
+let age2 = 21
+
+if age1 > 18 && age2 > 18 {
+    print("Both are over 18")
+}
+
+if age1 > 18 || age2 > 18 {
+    print("At least one of them is over 18")
+}
+
+
+//The Ternary Operator
+print(firstCard == secondCard ? "Cards are the same" : "Cards are different")
+
+if firstCard == secondCard {
+    print("Cards are the same")
+} else {
+    print("Cards are different")
+}
+
+
+//Switch Statments
+let weather = "sunny"
+
+
+//Swift will only run the code inside each case. If you want execution to continue on to the next case, use the fallthrough keyword
+switch weather {
+case "rain":
+    print("Bring an umbrella")
+case "snow":
+    print("Wrap up warm")
+case "sunny":
+    print("Wear sunscreen")
+    fallthrough
+default:
+    print("Enjoy your day!")
+}
+
+
+//Range Operators
+let score = 85
+
+switch score {
+case 0..<50:
+    print("You failed badly.")
+case 50..<85:
+    print("You did OK.")
+default:
+    print("You did great!")
+}
+
+
